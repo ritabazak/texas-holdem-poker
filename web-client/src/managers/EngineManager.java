efbeba6f5f3978cabfdd8632fb47615596c7d508
Amit Dahan <amit@payobin.com>
2018-02-03 13:26:07 +0200

    Removed old code used to test PokerEngine availability

--- web-client/src/managers/EngineManager.java
+++ web-client/src/managers/EngineManager.java
@@ -1,20 +1,15 @@
 package managers;
 
 import javax.servlet.ServletContext;
+import engine.PokerEngine;
+
 public class EngineManager {
-
-/*
     public static PokerEngine getEngineFromContext(ServletContext context) {
-
         if (context.getAttribute("ENGINE") == null) {
             context.setAttribute("ENGINE", new PokerEngine());
 
         }
 
         return (PokerEngine) context.getAttribute("ENGINE");
-    }*/
-    public void test() {
-        PokerEngine a = new PokerEngine();
-
     }
 }
